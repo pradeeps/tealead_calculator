0c67825b328d53e6e5d94c2fd26d08a33c77b7a2
Pradeep Susarla <pradeep@pradeeps.org>
2015-09-26 14:19:49 +0100

    calculator fixes on code review, rps game

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -1,72 +1,39 @@
-puts "Welcome to TeaLeaf Calculator App"
-puts "-------------------------"
+puts 'Welcome to TeaLeaf Calculator App'
+puts '-------------------------'
 
 loop do
-
-  puts "Please enter first number"
+  puts 'Please enter first number'
   num1 = gets.chomp
-  puts "Please enter second number"
+  puts 'Please enter second number'
   num2 = gets.chomp
 
-  operation_hash = {"1" => "Addition", "2" => "Subtraction", "3" => "Multiplication", "4" => "Division"}
-  puts "Please select from one of the operations below"
-  operation_hash.each do |k,v|
-    puts "#{k}.#{v}"
+  operation_hash = {
+    '1' => 'Addition',
+    '2' => 'Subtraction',
+    '3' => 'Multiplication',
+    '4' => 'Division' }
+  puts 'Please select from one of the operations below'
+  operations.each { |order, operation| puts "#{order}.#{operation}" }
+
+  operation_input = gets.chomp
+  until operation_hash.key?(operation_input)
+    puts 'Invalid operation, please enter a valid option '
+    operation_input = gets.chomp
   end
 
-  operation_input  = gets.chomp
-  while !operation_hash.has_key?(operation_input)
-    puts "Invalid operation, please enter a valid option "
-    operation_input  = gets.chomp
+  result = case operation_input
+           when '1'
+             num1.to_i + num2.to_i
+           when '2'
+             num1.to_i - num2.to_i
+           when '3'
+             num1.to_i * num2.to_i
+           when '4'
+             num1.to_f / num2.to_f
   end
+  puts "Result of #{operation_hash[operation_input]} of #{num1} and #{num2} is #{result} \n"
 
-  case operation_input
-    when '1'
-      result = num1.to_i + num2.to_i
-    when '2'
-      result = num1.to_i - num2.to_i
-    when '3'
-      result = num1.to_i * num2.to_i
-    when '4'
-      result = num1.to_f / num2.to_f
-  end
-      puts "Result of #{operation_hash[operation_input]} of #{num1} and #{num2} is #{result} \n"
-
-      puts "Would you like to perform an other operation? (Y/N)"
-      do_this_again = gets.chomp.capitalize
-      break unless do_this_again == 'Y'
+  puts 'Would you like to perform an other operation? (Y/N)'
+  do_this_again = gets.chomp.capitalize
+  break unless do_this_again == 'Y'
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
